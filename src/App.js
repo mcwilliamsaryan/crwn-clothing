1501121e80fc5ba6a485efb8bd3d1abbbe1870bf
Ryan McWilliams <cras.rmcwilliams@gmail.com>
2021-04-26 18:46:12 -0700

    Action Types
    Added UserActionTypes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux'
 
 import './App.css';
@@ -19,7 +19,9 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
+
     const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -28,10 +30,10 @@ class App extends React.Component {
           setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            })
+            });
           });
       };
-      setCurrentUser({ userAuth });
+      setCurrentUser( userAuth );
     });
   }
 
@@ -46,17 +48,28 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} /> 
-        <Route path='/signin' component={SignInAndSignUpPage} /> 
+        <Route 
+          exact 
+          path='/signin' 
+          render={() => 
+          this.props.currentUser ? 
+          (<Redirect to='/' />) 
+          : (
+          <SignInAndSignUpPage />)} /> 
       </Switch>
     </div>
     
     );
   }
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
   
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
